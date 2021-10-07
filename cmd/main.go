b95fdd3b7eadd81c801bea3248851e74fcdbe7d2
= <=>
2021-10-07 17:38:40 +0300

    implement logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,26 @@
 package main
 
-import "yet-another-restapi/internal/server"
+import (
+	"yet-another-restapi/internal/server"
+)
 
 const servePort string = ":8999"
 
 func main() {
-	s := &server.Server{
-		server.DefaultModel(),
-		&server.JsonPresenter{},
-		server.NewFileLogger("data/log"),
-		servePort,
-	}
-	s.Run()
+
+	l, _ := server.NewMultiLogger("data/log2")
+	l.Debug("Ahaha1", "lol1")
+	l.Info("Ahaha2", "lol2")
+	l.Warn("Ahaha3", "lol3")
+
+
+	//
+	//s := &server.Server{
+	//	server.DefaultModel(),
+	//	&server.JsonPresenter{},
+	//	server.NewFileLogger("data/log"),
+	//	servePort,
+	//}
+	//s.Run()
 
 }
--- internal/server/log.go
+++ internal/server/log.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -39,4 +40,54 @@ func (l FileLogger) Info(v ...interface{}) {
 
 func (l FileLogger) Warn(v ...interface{}) {
 	l.Print("(WW) ", fmt.Sprintln(v...))
+}
+
+type MultiLogger struct {
+	files map[string]io.Writer
+}
+
+func NewMultiLogger(filename string) (*MultiLogger, error) {
+	mainLogFile, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return &MultiLogger{}, err
+	}
+
+	return &MultiLogger{
+		map[string]io.Writer{
+			"main": mainLogFile,
+		},
+	}, nil
+}
+
+
+func (l MultiLogger) Print(out []io.Writer, v ...interface{}) {
+	for _, flow := range out {
+		_, err := fmt.Fprint(flow, v...)
+		if err != nil {
+			log.Println("(WW) Error while logging:", err)
+		}
+	}
+}
+
+func (l MultiLogger) Debug(v ...interface{}) {
+	flows := []io.Writer{os.Stdout}
+	l.Print(flows, "(--) ", fmt.Sprintln(v...))
+}
+
+
+func (l MultiLogger) Info(v ...interface{}) {
+	flows := []io.Writer{
+		os.Stdout,
+		l.files["main"],
+	}
+	l.Print(flows, "(II) ", fmt.Sprintln(v...))
+}
+
+
+func (l MultiLogger) Warn(v ...interface{}) {
+	flows := []io.Writer{
+		os.Stdout,
+		l.files["main"],
+	}
+	l.Print(flows, "(WW) ", fmt.Sprintln(v...))
 }
